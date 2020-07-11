88247af5ce62b6ca64fd88270e1d78a1d014e3d1
maui <maui@devf.mx>
2020-07-10 19:47:55 -0500

    Ajustar despliegue de app para mostrar los nuevos componentes mediante el enrutador

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+// Components
 import Navigation from './components/Navigation';
-import HolaMundo from './components/HolaMundo';
+import Footer from './components/Footer';
+
+// Contexts
 import AuthContextProvider from './contexts/AuthContext';
+
+// Misc
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Container } from 'reactstrap';
+import Routes from './Routes';
 
 function App() {
   return (
-    <AuthContextProvider>
-      <Navigation />
-      <div className="container">
-        <h1>Hola Cinta Negra</h1>
-        <HolaMundo />
-      </div>
-    </AuthContextProvider>
+    <Fragment>
+      <Router>
+        <AuthContextProvider>
+            <Navigation />
+            <Container className="mt-4">
+              <Switch>
+                { Routes }
+              </Switch>
+            </Container>
+            <Footer />
+        </AuthContextProvider>
+      </Router>
+    </Fragment>
   );
 }
 
