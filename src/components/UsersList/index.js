27f5e2718aa498380f99e1f44b2aacea1ae5b18b
maui <maui@devf.mx>
2020-07-13 21:49:54 -0500

    Proteger ruta de /users de usuarios sin sesion activa

--- src/components/UsersList/index.js
+++ src/components/UsersList/index.js
@@ -1,6 +1,12 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const UsersList = () => {
+  const { isAuth } = useContext(AuthContext);
+
+  if (!isAuth) return <Redirect to="/login" />
+
   return ( 
     <Fragment>
       <h2>Lista de Usuarios</h2>
